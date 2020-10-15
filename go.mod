64a041edd0c76edfb5f46a2f45e5ad2a106e9103
koketama <60101042+koketama@users.noreply.github.com>
2020-10-15 23:31:07 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/koketama/zaplog
+
+go 1.15
+
+require (
+	github.com/pkg/errors v0.9.1
+	go.uber.org/zap v1.16.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
---
+++ zaplog_test.go
@@ -0,0 +1,20 @@
+package zaplog
+
+import (
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestJSONLogger(t *testing.T) {
+	logger, err := NewJSONLogger(WithField("name", "minami"), WithFileP("/tmp/zap/dummy/zaplog.log"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logger.Sync()
+
+	err = errors.New("pkg error")
+	logger.Error("err occurs", WrapMeta(nil, NewMeta("para1", "value1"), NewMeta("para2", "value2"))...)
+	logger.Error("err occurs", WrapMeta(err, NewMeta("para1", "value1"), NewMeta("para2", "value2"))...)
+
+}
